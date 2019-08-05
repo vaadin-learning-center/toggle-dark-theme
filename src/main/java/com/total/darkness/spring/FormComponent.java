b78b36ceab094b0930d81b2be46ca7ffa459c473
Sven Ruppert <sven.ruppert@gmail.com>
2019-08-05 08:56:08 +0200

    update to V14RC/, NPM mode, using plain MessageBean without Springmagic

--- src/main/java/com/total/darkness/spring/FormComponent.java
+++ src/main/java/com/total/darkness/spring/FormComponent.java
@@ -3,21 +3,24 @@ package com.total.darkness.spring;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 
-public class FormComponent extends VerticalLayout {
+
+public class FormComponent
+    extends VerticalLayout {
+
 
   public FormComponent() {
     setSpacing(false);
     setWidth(null);
-    add(
-        new TextField("Name"),
+    final Button button = new Button("Sign up");
+    button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    button.addClickListener((e) -> Notification.show(new MessageBean().getMessage()));
+    add(new TextField("Name"),
         new EmailField("Email"),
-        new DatePicker("DOB"),
-        new Button("Sign up"){{addThemeVariants(ButtonVariant.LUMO_PRIMARY);}}
-    );
+        new DatePicker("DOB"), button);
   }
 }
